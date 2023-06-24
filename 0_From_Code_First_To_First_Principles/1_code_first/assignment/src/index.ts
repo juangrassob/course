f3a70d3d3962e51d80f193e79c56e80bc5230587
Juan Grasso <juangrassob@gmail.com>
2023-06-24 14:24:53 -0300

    request's validations

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
@@ -5,13 +5,47 @@ const app = express();
 app.use(express.json());
 
 app.post("/user/new", (req, res) => {
-  res.status(201).json({ success: true });
+  const { email, username, firstName, lastName } = req.body.data;
+
+  if (!email || !username || !firstName || !lastName) {
+    return res
+      .status(400)
+      .json({ success: false, error: "Missing mandatory fields." });
+  }
+
+  return res.status(201).json({ success: true });
 });
+
 app.put("/user/edit/:userId", (req, res) => {
-  res.status(200).json({ success: true });
+  const { email, username, firstName, lastName } = req.body.data;
+
+  if (email) {
+    return res
+      .status(400)
+      .json({ success: false, error: "You can't modify the email field." });
+  }
+
+  if (!username && !firstName && !lastName) {
+    return res.status(400).json({
+      success: false,
+      error: "You must at least modify one field.",
+    });
+  }
+
+  return res.status(200).json({ success: true });
 });
+
 app.get("/user", (req, res) => {
-  res.status(200).json({ success: true, data: { user: {} } });
+  const email = req.query.email;
+
+  if (!email) {
+    return res.status(400).json({
+      success: false,
+      error: "You must specify a user email.",
+    });
+  }
+
+  return res.status(200).json({ success: true, data: { user: {} } });
 });
 
 const APP_PORT = 3000;
