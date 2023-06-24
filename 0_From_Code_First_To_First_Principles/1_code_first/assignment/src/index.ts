df96632348029f2d15891cd170834f1bdd441b34
Juan Grasso <juangrassob@gmail.com>
2023-06-24 13:37:56 -0300

    basic routes and controllers

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
@@ -4,6 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+app.post("/user/new", (req, res) => {
+  res.status(201).json({ success: true });
+});
+app.put("/user/edit/:userId", (req, res) => {
+  res.status(200).json({ success: true });
+});
+app.get("/user", (req, res) => {
+  res.status(200).json({ success: true, data: { user: {} } });
+});
+
 const APP_PORT = 3000;
 const APP_URL = "127.0.0.1";
 
