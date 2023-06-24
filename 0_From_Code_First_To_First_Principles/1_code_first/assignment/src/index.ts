ed6f18cdb6571b1e33e578d144eeab3b64178b7c
Juan Grasso <juangrassob@gmail.com>
2023-06-24 15:41:53 -0300

    moved port and url to env variables

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
@@ -96,8 +96,8 @@ app.get("/user", async (req, res) => {
   return res.status(200).json({ success: true, data: { user } });
 });
 
-const APP_PORT = 3000;
-const APP_URL = "127.0.0.1";
+const APP_URL = process.env.APP_URL;
+const APP_PORT = process.env.APP_PORT;
 
 app.listen(APP_PORT, () =>
   console.log(`App listening at: http://${APP_URL}:${APP_PORT}`)
