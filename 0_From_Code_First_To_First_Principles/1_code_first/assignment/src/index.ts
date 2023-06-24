3492019985763475eb1e851a48881289e14b60b6
Juan Grasso <juangrassob@gmail.com>
2023-06-24 15:35:42 -0300

    endpoints data persistence using prisma

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
@@ -16,39 +16,84 @@ app.post("/user/new", async (req, res) => {
       .json({ success: false, error: "Missing mandatory fields." });
   }
 
+  const conflictUsers = await prisma.user.findMany({
+    where: { email, username },
+  });
+
+  if (conflictUsers.length) {
+    return res.status(400).json({
+      success: false,
+      error: "The email or username are already taken.",
+    });
+  }
+
+  await prisma.user.create({
+    data: { email, username, firstName, lastName },
+  });
+
   return res.status(201).json({ success: true });
 });
 
-app.put("/user/edit/:userId", (req, res) => {
+app.put("/user/edit/:userId", async (req, res) => {
+  const userId = Number(req.params.userId);
   const { email, username, firstName, lastName } = req.body.data;
 
-  if (email) {
-    return res
-      .status(400)
-      .json({ success: false, error: "You can't modify the email field." });
+  if (email || username) {
+    return res.status(400).json({
+      success: false,
+      error: "You can't modify the email and username fields.",
+    });
   }
 
-  if (!username && !firstName && !lastName) {
+  if (!firstName && !lastName) {
     return res.status(400).json({
       success: false,
       error: "You must at least modify one field.",
     });
   }
 
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      error: "The user was not found",
+    });
+  }
+
+  await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      firstName,
+      lastName,
+    },
+  });
+
   return res.status(200).json({ success: true });
 });
 
-app.get("/user", (req, res) => {
-  const email = req.query.email;
-
-  if (!email) {
+app.get("/user", async (req, res) => {
+  if (!req?.query?.email) {
     return res.status(400).json({
       success: false,
       error: "You must specify a user email.",
     });
   }
 
-  return res.status(200).json({ success: true, data: { user: {} } });
+  const email = String(req.query.email);
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      error: "The user was not found",
+    });
+  }
+
+  return res.status(200).json({ success: true, data: { user } });
 });
 
 const APP_PORT = 3000;
